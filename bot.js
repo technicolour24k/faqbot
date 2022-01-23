7b58c5aa2c5f7d30619da25e30672e5a3d4b36ba
TonyJ <hitetsu@hotmail.com>
2022-01-23 13:23:29 +0000

    Mentions test

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-// Run dotenv
 require('dotenv').config();
 
 const { Client, Intents } = require('discord.js');
@@ -6,26 +5,30 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.username}!`);
 });
 
 client.on('messageCreate', msg => {
-    // switch (msg) {
-    //     case 'ping':
-    //         bot.sendMessage({
-    //             to: channelID,
-    //             message: 'Pong!'})
-    //     case 'nom':
-    //         bot.sendMessage({
-    //             to: channelID,
-    //             message: 'Eating!'
-    //         })
-    //     break;
-    // }
-    if (msg.content === 'ping') {
-       msg.reply('Pongo!');
-     }
-  });
-
-
+     if (msg.content == 'ping') {
+        msg.reply(`${msg.content}? Alright, pong!`)
+    }
+    
+    if (msg.mentions.has(client.user.id)) {
+        switch (msg.content) {
+            case "Fred": 
+                msg.reply(`Fred? Freddos? They're expensive now :(`)
+                break;
+            case "John":
+                msg.reply(`John, John, the bakers son. Fell on the floor and landed on a nun`)
+                break;
+            default:
+                    // do something
+                } 
+            }
+            
+                if (msg.mentions.has(client.user.id)) {
+                    msg.channel.send("Hello there!");
+                }
+        });
 
 client.login(process.env.TOKEN);
