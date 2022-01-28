04bba71b61d43b095a8325dad6622eab91b67778
TonyJ <hitetsu@hotmail.com>
2022-01-28 12:26:07 +0000

    Added some comments

--- bot.js
+++ bot.js
@@ -12,51 +12,51 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', msg => {
-    if (debug) {console.log(`Message received: ${msg}`)}
+    if (debug) {console.log(`Message received: ${msg}`)} //Confirm message received by bot
     message = msg.content;
-    if (debug) {console.log(`Message transferred: ${message}`)}
+    if (debug) {console.log(`Message transferred: ${message}`)} //duplicate message into another variable for manipulation
     message.toString();
-    if (debug) {console.log(`Message to String: ${message}`)}
+    if (debug) {console.log(`Message to String: ${message}`)} //switch message to string for lowercase later
     message = message.toLowerCase();
-    if (debug) {console.log(`Message to lowercase: ${message}`)}
+    if (debug) {console.log(`Message to lowercase: ${message}`)} //lowercase message to avoid case sensitive issues in checking messages
 
-    if (!msg.author.bot) {
+    /*make sure we aren't trying to get the bot to reply to it's own message
+    because otherwise we end up in infinite loops and that sounds like a square enix sort of grind*/
+    if (!msg.author.bot) { 
         response = msgResponse(message);
         if (debug) {console.log(`response: ${response}`)}
-        if ((response) || ((!response==="") && (!response==undefined)))
+        if ((response) || ((!response==="") && (!response==undefined))) //if a response exists then reply with it
             msg.reply(response);
     }
 });
 
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN); //prep all this jazz and then log in the bot
 
 function msgResponse(msg) {
-    let reply = "";
-    let commandList = "";
-    const questions = {
+    let reply = ""; //craft a blank reply to stop us from responding with old reply
+    let commandList = ""; //create a blank command list to populate with questions
+    const questions = { //the list of things we want to reply to, and how we want to reply
         "pol-0033":"Try turning your computer off, and restarting your router - Take the plug out, leave it for 30 seconds, and then plug it all back in.",
         "test":"You wanna test? That'll be fun!",
         "pancakes":"Let's eat!"
     }
 
-    for (const [key, value] of Object.entries(questions)) {
+    for (const [key, value] of Object.entries(questions)) { //for everything in the "questions" object,
         if (debug) {console.log(`KEY CHECK: ${key}: ${value}`);}
-
-        
-        if(msg.includes("available keywords")) {
+        if(msg.includes("available keywords")) { //check to see if someone needs help
             for (const [key, value] of Object.entries(questions)) {
-                if (commandList == "") {commandList = key} else {
+                if (commandList == "") {commandList = key} else { //staple together a command list
                     commandList = commandList + `, ${key}`;
                 }
             }
-            reply = `Available keywords: ${commandList}`;
+            reply = `Available keywords: ${commandList}`; //send the reply and break out of the loop
             break;
         }
-        else if (msg.includes(key)) {
-            if (debug) {console.log(`KEY CHECK2: ${key}: ${value}`);}
-            reply = `[**Keyword found: ${key}**] ${value}.
-*Wrong one? Try saying "available keywords" to see my list of keywords.*`
+        else if (msg.includes(key)) { //if someone's not asking for help, check to see if the message includes one of the question keys
+            if (debug) {console.log(`KEY CHECK2: ${key}: ${value}`);} 
+            reply = `[**Keyword found: ${key}**] ${value}. 
+*Wrong one? Try saying "available keywords" to see my list of keywords.*` //reply with the answer to the keyword we find, and then break out of the loop
             break;
         }
       }
@@ -64,6 +64,6 @@ function msgResponse(msg) {
     if (debug) {console.log(`Function Message: ${msg}`)}
     if (debug) {console.log(`Function Reply: ${reply}`)}
 
-    if (reply) { return reply; }
+    if (reply) { return reply; } //if a reply exists (keywords have been found), then send it - otherwise return false
     else { return false; }
 }
